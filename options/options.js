c50e985164dea6e6f058a8e70fab475213b14390
Crowdbotics <team@crowdbotics.com>
2021-08-03 04:26:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "quiet_cherry_29374Identifier",
+  url: "https://quiet_cherry_29374Identifier.botics.co",
+  api: "https://quiet_cherry_29374Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
